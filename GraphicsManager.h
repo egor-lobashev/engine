11500710d357d4b676499c7a683d56ea70f834ec
Kudaxech <krylov200010@mail.com>
2020-03-13 00:27:44 +0300

    Ass

--- GraphicsManager.h
+++ GraphicsManager.h
@@ -1,1 +1,25 @@
-// Хацкер поработал
+#ifndef __GRAPHICS_MANAGER__
+#define __GRAPHICS_MANAGER__
+
+
+#include <SFML/Graphics.hpp>
+
+class GraphicsManager {
+    public:
+        void DrawAll(sf::RenderWindow& window , std::vector <GameObject*> all_objects){
+
+            for (auto object : all_objects){
+                if( object -> getComponent<Renderer>() != nullptr ){
+
+                    window.draw( *( object -> getComponent<Renderer>() -> getSprite() )  );
+
+                }
+            }
+
+            window.display();
+        }
+
+    private:
+};
+
+#endif
