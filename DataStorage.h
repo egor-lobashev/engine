241b7ecdd7f124b2de84b725a8b6f25d29a7293e
kudaxech <55214057+kudaxech@users.noreply.github.com>
2020-03-14 15:55:45 +0300

    Update DataStorage.h

--- DataStorage.h
+++ DataStorage.h
@@ -1,3 +1,7 @@
+#ifndef __DATA_STORAGE__
+#define __DATA_STORAGE__
+
+
 #include <string>
 #include <map>
 #include <vector>
@@ -6,35 +10,50 @@
 class DataStorage{
     public:
 
-        bool addObject(std::string object_name, GameObject& object){
+        ~DataStorage(){
 
-            objects[object_name] = object;
-            return true;
+            for(auto item : map_of_game_objects){
+                delete[] map_of_game_objects[item.first];
+            }
+
         }
-        bool deleteObject(std::string object_name){
-            if(objects.erase(object_name)){
+
+        bool addObject(std::string object_name , GameObject* obj){
+
+            if(map_of_game_objects[object_name] = obj){
                 return true;
             }
 
             return false;
         }
-        GameObject& getObject(std::string object_name){
-            return objects[object_name];
+        bool deleteObject(std::string object_name){
+            if(map_of_game_objects.count(object_name) == 1){
+
+                delete map_of_game_objects[object_name];
+                map_of_game_objects.erase(object_name);
+                return true;
+            }
+            return false;
+        }
+        GameObject* getObject(std::string object_name){
+            return map_of_game_objects[object_name];
         }
 
         std::vector <GameObject*> getAll(){
             
             std::vector <GameObject*> output_vector;
-            for(auto item : objects){
-                output_vector.push_back(&item.second);
+            for(auto item : map_of_game_objects){
+                output_vector.push_back(item.second);
             }
-            return output_vector;
 
+            return output_vector;
         }
 
 
     private:
 
-    std::map<std::string,GameObject> objects;
+    std::map<std::string,GameObject*> map_of_game_objects;
 
 };
+
+#endif
